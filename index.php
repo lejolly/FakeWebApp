346bd19a5b2b5a1209b7318077d1b6917cd0988b
Joshua Lee <me@jolly.sg>
2016-08-23 22:47:34 +0800

    added index.php and db.php.sample

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Random Movies</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/picnicss/6.1.4/picnic.min.css">
+</head>
+<body>
+<div class="flex">
+<div class="off-fourth-400"><span></span></div>
+<div class="full third-800"><span><h1 style="text-align:center;">Random Movies</h1>
+<?php
 require_once 'medoo.php';
+require_once 'db.php';
 
-echo 'Hello World';
+$datas = $database->query("
+	SELECT title, description
+	FROM `sakila`.`film`
+	ORDER BY RAND()
+	LIMIT 5;
+");
+
+foreach($datas as $data)
+{
+	echo "<article class=\"card\"><header><h3>";
+	echo $data["title"];
+	echo "</h3></header><footer>";
+	echo $data["description"];
+	echo "</footer></article>";
+}
+
+?>
+</span></div>
+<div class="off-fourth-400"><span></span></div>
+</div>
+</body>
+</html>
